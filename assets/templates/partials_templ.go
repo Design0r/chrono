817fadf828097fe79a2e3df4a7d8b09cd1201a68
Kannyi <45148772+Kannyi@users.noreply.github.com>
2025-01-29 13:29:01 +0100

    fixed: user cannot delete accepted vacation days anymore

--- assets/templates/partials_templ.go
+++ assets/templates/partials_templ.go
@@ -86,7 +86,7 @@ func Event(event schemas.Event, user repo.User) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		if event.UserID == user.ID {
+		if event.UserID == user.ID && event.State != "accepted" || user.IsSuperuser == true {
 			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 7, "<span><button hx-swap=\"delete\" hx-target=\"")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err

d0596c4e7d9b8d11c00fa2a96a655e288827bfbf
alex <alex.mors@gmx.de>
2025-01-30 10:49:52 +0100

    enabled event delete button on non vacation events

--- assets/templates/partials_templ.go
+++ assets/templates/partials_templ.go
@@ -86,7 +86,7 @@ func Event(event schemas.Event, user repo.User) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		if event.UserID == user.ID && event.State != "accepted" || user.IsSuperuser == true {
+		if (event.UserID == user.ID && event.State != "accepted") || !service.IsVacation(event.Name) || user.IsSuperuser {
 			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 7, "<span><button hx-swap=\"delete\" hx-target=\"")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err

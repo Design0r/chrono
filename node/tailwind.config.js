a346b5ada3927bbda61b46ffd51ab114f4a71443
alex <alex.mors@gmx.de>
2025-02-03 10:17:04 +0100

    removing unused daisyui themes

--- node/tailwind.config.js
+++ node/tailwind.config.js
@@ -1,49 +1,17 @@
-import daisyui from 'daisyui'
+import daisyui from "daisyui";
 export default {
-	content: ['../**/*.html', '../**/*.templ', '../**/*.go'],
-	theme: {},
-	plugins: [daisyui],
-	daisyui: {
-		themes: [
-			{
-				dim: {
-					...require('daisyui/src/theming/themes')['dim'],
-					info: '#9ED0F1',
-					error: '#fe7c5d',
-				},
-			},
-			'light',
-			'dark',
-			'cupcake',
-			'bumblebee',
-			'emerald',
-			'corporate',
-			'synthwave',
-			'retro',
-			'cyberpunk',
-			'valentine',
-			'halloween',
-			'garden',
-			'forest',
-			'aqua',
-			'lofi',
-			'pastel',
-			'fantasy',
-			'wireframe',
-			'black',
-			'luxury',
-			'dracula',
-			'cmyk',
-			'autumn',
-			'business',
-			'acid',
-			'lemonade',
-			'night',
-			'coffee',
-			'winter',
-			'dim',
-			'nord',
-			'sunset',
-		],
-	},
-}
+  content: ["../**/*.html", "../**/*.templ", "../**/*.go"],
+  theme: {},
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        dim: {
+          ...require("daisyui/src/theming/themes")["dim"],
+          info: "#9ED0F1",
+          error: "#fe7c5d",
+        },
+      },
+    ],
+  },
+};

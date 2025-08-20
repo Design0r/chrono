0ebf9850999d2dbb71d223f3f502dce7396b3a76
alex <alex.mors@gmx.de>
2025-08-20 23:19:08 +0200

    refactor: change name

--- assets/templates/team_templ.go
+++ assets/templates/team_templ.go
@@ -561,7 +561,7 @@ func TeamRow(currUser domain.User, user domain.UserWithVacation, form bool) temp
 				if templ_7745c5c3_Err != nil {
 					return templ_7745c5c3_Err
 				}
-				templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 47, "\" hx-swap=\"outerHTML\" hx-target=\"closest tr\">edit</button></td>")
+				templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 47, "\" hx-swap=\"outerHTML\" hx-target=\"closest tr\">Edit</button></td>")
 				if templ_7745c5c3_Err != nil {
 					return templ_7745c5c3_Err
 				}

dfa8c9c3357f800c6470a2fd8a4d5fecbd2b684b
alex <alex.mors@gmx.de>
2025-12-09 09:51:26 +0100

    fix: fixed prod chrono url

---
+++ frontend/dist/assets/_auth-_PiF_MN9.js
@@ -0,0 +1,1 @@
+import{c as o,j as s,O as n}from"./index-ulRjAiTq.js";function c(){const e=o.c(1);let t;return e[0]===Symbol.for("react.memo_cache_sentinel")?(t=s.jsx(n,{}),e[0]=t):t=e[0],t}export{c as component};
--- frontend/dist/assets/_auth._admin-DzZ6gV2Q.js
+++
@@ -1,1 +0,0 @@
-import{c as o,j as n,O as s}from"./index-DZ392lUP.js";function r(){const e=o.c(1);let t;return e[0]===Symbol.for("react.memo_cache_sentinel")?(t=n.jsx(s,{}),e[0]=t):t=e[0],t}export{r as component};

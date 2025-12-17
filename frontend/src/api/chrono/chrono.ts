ab37764688b26ac38b871f41ac7fbe0d9e9cd240
alex <alex.mors@gmx.de>
2025-12-17 21:17:10 +0100

    fix: reverting chronoapi url

--- frontend/src/api/chrono/chrono.ts
+++ frontend/src/api/chrono/chrono.ts
@@ -1,1 +1,1 @@
-export const CHRONO_URL = `${import.meta.env.DEV ? "http://localhost:8080" : "https://chronoapi.theapic.com"}/api/v1`;
+export const CHRONO_URL = `${import.meta.env.DEV ? "http://localhost:8080" : ""}/api/v1`;
